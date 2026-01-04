d8bd9c9b5da9a2a022896ebca25240a455fa8b09
Илья <ilfd20070916@gmail.com>
2026-01-04 14:07:34 +0100

    I am complete with tasks 1 to 5

---
+++ main.js
@@ -0,0 +1,53 @@
+let qwe = 1;
+console.log(qwe);
+var variable = '1231';
+console.log(variable);
+
+const bool = false;
+console.log(bool);
+
+let unundead = undefined;
+
+console.log(unundead);
+
+var variable = '3333'
+console.log(variable);
+
+let str = '123'
+console.log(str);
+
+let num123 = +str
+console.log(num123)
+
+let number132 = parseInt(str)
+console.log(number132)
+
+let num = 100;
+console.log(num);
+let num1 = String(num);
+console.log(num1);
+// ============================== 4 ЗАДАНИЕ =-============
+
+let number4 = 100;
+let num5 = '50';
+console.log(num5 + number4);
+
+let num6 = 200 ;
+console.log(num6 + number4);
+let num7 = true
+console.log(num7 + number4);
+let num8 = undefined ;
+console.log(num8 + number4);
+let num9 = 'Hello'
+console.log(num9 + number4);
+console.log(num7 + num9);
+console.log(num9 + num8);
+console.log(num9 - number4);
+console.log(num7 - num9);
+console.log(num9 - num8);
+console.log(num8 - number4);
+console.log(num7 - number4);
+console.log(num6 - number4);
+console.log(num5 - number4);
+
+
